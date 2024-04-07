9ea0e6be5e20b214556cfd6ce8734d76acf2bb9a
Krooshk <kirill.krush@mail.ru>
2024-04-07 13:09:23 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -10,12 +10,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "../client")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/index.html"));
-});
-
 app.use(bodyParser.json());
 app.use("/api/v1/", v1currencyRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
