38ccc314bfc5c60202bf784b6d3539f4b3058e63
Krooshk <kirill.krush@mail.ru>
2024-04-07 12:55:50 +0300

    попытка прикрутить frontend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 // In src/index.js
 const express = require("express");
+const path = require("path");
 const v1currencyRouter = require("./V1/routes/currencyRoutes");
 const bodyParser = require("body-parser");
 const { startDatabase } = require("./database/data");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/index.html"));
+});
 
 app.use(bodyParser.json());
 app.use("/api/v1/", v1currencyRouter);
